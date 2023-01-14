d210246620c0a6f28c48763d64b377f8623a520e
Paweł Zaremba <zarembapawel95@gmail.com>
2023-01-14 16:37:54 +0100

    check file exist and validate path

--- src/main/java/pl/zarembapawel/backuper/service/InitService.java
+++ src/main/java/pl/zarembapawel/backuper/service/InitService.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Service;
 import pl.zarembapawel.backuper.enums.GroupType;
 import pl.zarembapawel.backuper.model.Group;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 @Service
@@ -15,7 +18,7 @@ public class InitService {
 
     public Group iniGroup(GroupType type) {
         String path = getPath(type);
-        if(!pathExist(path)) {
+        if(!pathExist(path) || !pathValid(path)) {
             logger.error("Path {} don't exits!", path);
             return iniGroup(type);
         }
@@ -29,6 +32,16 @@ public class InitService {
     }
 
     private boolean pathExist(String path) {
+        Path filePath = Paths.get(path);
+        return Files.exists(filePath);
+    }
+
+    private boolean pathValid(String path) {
+        if(!path.contains("/") && !path.contains("\\")) {
+            return false;
+        } else if (path.length() < 5) {
+            return false;
+        }
         return true;
     }
 }
